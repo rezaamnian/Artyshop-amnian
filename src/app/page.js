7ebf67e5b3bb81a449bbed839deee9ec3a82acdb
kavir <rezaamnian1373@gmail.com>
2025-07-29 01:28:49 -0800

    header-style

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
+import Header from "./components/header/Header";
 
 export default function Home() {
   return (
     <div>
-      <h1>kermanshah</h1>
-      <h1>tehran</h1>
-      <h1>tabriz</h1>
+      
+      <Header/>
     </div>
-  );
+  )
 }
